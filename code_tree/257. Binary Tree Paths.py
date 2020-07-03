25a06ead2983feaefc481999230a9729491d8f47
ShuJui Chang <au6su3y@gmail.com>
2020-07-03 15:40:25 -0700

    Create 257. Binary Tree Paths.py

---
+++ code_tree/257. Binary Tree Paths.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def binaryTreePaths(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[str]
+        """
+        res = []
+        if not root:
+            return res
+        if not root.left and not root.right:
+            res.append(str(root.val))
+            return res
+        for path in self.binaryTreePaths(root.left):
+            res.append(str(root.val)+ '->' + path)
+        for path in self.binaryTreePaths(root.right):
+            res.append(str(root.val)+ '->' + path)
+        return res
