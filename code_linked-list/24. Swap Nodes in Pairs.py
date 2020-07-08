81994558060bf42eef661f22573fcb754f5146e4
ShuJui Chang <au6su3y@gmail.com>
2020-07-08 14:23:27 -0700

    Create 24. Swap Nodes in Pairs.py

---
+++ code_linked-list/24. Swap Nodes in Pairs.py
@@ -0,0 +1,23 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def swapPairs(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        if not head or not head.next:
+            return head
+        pre = new_head = ListNode(0)
+        while head and head.next:
+            tmp = head.next
+            head.next = tmp.next
+            tmp.next = head
+            pre.next = tmp
+            pre = head
+            head = head.next
+        return new_head.next
