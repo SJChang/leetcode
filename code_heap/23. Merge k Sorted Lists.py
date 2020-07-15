2f085c320ea8b9d1e748d7328ad43c7542256dc9
ShuJui Chang <au6su3y@gmail.com>
2020-07-15 11:18:27 -0700

    Create 23. Merge k Sorted Lists.py

---
+++ code_heap/23. Merge k Sorted Lists.py
@@ -0,0 +1,31 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def mergeKLists(self, lists):
+        """
+        :type lists: List[ListNode]
+        :rtype: ListNode
+        """
+        heap = []
+        for node in lists:
+            if node:
+                heap.append((node.val,node))
+        
+        heapq.heapify(heap)
+        
+        
+        head = ListNode(0); curr=head
+        
+        while heap:
+            pop=heapq.heappop(heap)
+            curr.next=ListNode(pop[0])
+            curr = curr.next
+            if pop[1].next:
+                heapq.heappush(heap,(pop[1].next.val,pop[1].next))
+            
+        return head.next
+        
