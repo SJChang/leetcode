3ca7b9f8f67a4d14d31c9488202c452ab3a40635
ShuJui Chang <au6su3y@gmail.com>
2020-07-16 14:59:17 -0700

    Create 18. 4Sum.py

---
+++ hash-table/18. 4Sum.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def fourSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[List[int]]
+        """
+        resultList = []
+        nums.sort()
+        for i in range(0,len(nums)-3):
+            for j in range(i+1,len(nums)-2):
+                p = j+1
+                q = len(nums)-1
+                while p != q:
+                    summer = nums[i]+nums[j]+nums[p]+nums[q]
+                    if summer == target:
+                        listT = [nums[i], nums[j], nums[p], nums[q]]
+                        if listT not in resultList:
+                            resultList.append(listT)
+                        p = p + 1
+                    elif summer > target:
+                        q = q-1
+                    else:
+                        p = p+ 1
+        return resultList
